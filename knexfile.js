85e956f22f43c177a8c8a50ed54a0067cc066d7a
David Moates <david.m.moates@gmail.com>
2020-04-28 21:40:00 -0500

    fixed my bugs

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
 	client: "sqlite3",
 	useNullAsDefault: true,
 	connection: {
-		filename: "./data/auth.db3",
+		filename: "./data/users.db3",
 	},
 	migrations: {
 		directory: "./data/migrations",
